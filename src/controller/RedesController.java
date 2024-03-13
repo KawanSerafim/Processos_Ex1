5ce302647b144ae0ac923c5d4e1c14ca574fb200
Kawan Serafim <kawanserafimdesouza@hotmail.com>
2024-03-12 22:27:24 -0300

    Commit 2.2

--- src/controller/RedesController.java
+++ src/controller/RedesController.java
@@ -102,10 +102,8 @@ public class RedesController {
 						for (String palavra : media) {
 							if (palavra.contains("dia")) {
 								
-								Process p2 = Runtime.getRuntime().exec("cls");
-								InputStream fluxo2 = p.getInputStream();
-								
 								System.out.println("\n" + palavra);
+								
 							}
 						}
 						
@@ -150,9 +148,6 @@ public class RedesController {
 								for (String palavra2 : div2) {
 									
 									if (cont == 2) {
-										
-										Process p2 = Runtime.getRuntime().exec("cls");
-										InputStream fluxo2 = p.getInputStream();
 										
 										System.out.println("\n A media deu: " + palavra2);
 										break;
